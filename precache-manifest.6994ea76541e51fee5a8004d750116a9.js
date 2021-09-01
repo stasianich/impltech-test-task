8d46a7cc3b3dd37acadda64477d6c55c5ba2aaad
Stas Pechenyi <spechenuy@gmail.com>
2021-09-01 14:31:55 +0300

    Updates

---
+++ precache-manifest.6994ea76541e51fee5a8004d750116a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ca88be35507b97797dceeabd6d72b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81210e11afbdd7bb3778",
+    "url": "./static/css/main.b5e56f77.chunk.css"
+  },
+  {
+    "revision": "378be3a1d81161d0a77a",
+    "url": "./static/js/2.d03213dd.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.d03213dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81210e11afbdd7bb3778",
+    "url": "./static/js/main.84f7b258.chunk.js"
+  },
+  {
+    "revision": "e07507b508c959f686be",
+    "url": "./static/js/runtime-main.5b92b18a.js"
+  }
+]);
